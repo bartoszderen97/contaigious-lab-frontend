bda40054f6eff6380b1c8e62d472e5baa7723e2d
bartoszderen97 <bartonia@wp.pl>
2020-05-11 23:06:11 +0200

    zaimplementowano backendowe mechanizmy wyszukiwania ofert z bazy danych

--- src/Offer.js
+++ src/Offer.js
@@ -1,8 +1,52 @@
 import React from 'react';
 import './Offer.css';
+import axios from "axios";
 
 class Offer extends React.Component {
+
+    constructor() {
+        super();
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    state = {
+        filteredExaminations: [],
+        examName: ""
+    };
+
+    handleChange(event) {
+        this.setState({examName:event.target.value});
+    }
+
+    handleSubmit(event) {
+        axios.
+        get('http://localhost/public/api/examination/getByName/'+this.state.examName).
+        then(response => {
+            if (response.status === 200){
+                this.setState({filteredExaminations:response.data.data});
+            }
+        }).
+        catch(error => {
+
+        });
+        event.preventDefault();
+    }
+
+    componentDidMount() {
+
+        axios.
+        get('http://localhost/public/api/examination/getAll').
+        then(response => {
+            if (response.status === 200){
+                this.setState({filteredExaminations:response.data.data});
+            }
+        })
+    }
+
     render() {
+
         return (
 
             <div className="container text-center">
@@ -12,57 +56,26 @@ class Offer extends React.Component {
                 </p>
                 <form className="form-inline mr-auto mb-5">
                     <div className="container text-center">
-                        <input className="form-control mr-sm-2" type="text" placeholder="Wpisz tutaj nazwe badania..."/>
+                        <input className="form-control mr-sm-2" type="text" onChange={this.handleChange} placeholder="Wpisz tutaj nazwe badania..."/>
                         <button id="offer_search_btn" className="btn btn-primary btn-rounded btn-lg my-0"
-                                type="submit">Wyszukaj
+                                type="button" onClick={this.handleSubmit}>Wyszukaj
                         </button>
                     </div>
                 </form>
-
                 <div className="card-deck mb-4 text-center">
-                    <div className="card mb-4 box-shadow">
-                        <div className="card-header">
-                            <h4 className="my-0 font-weight-normal">COVID-19</h4>
-                        </div>
-                        <div className="card-body">
-                            <h1 className="card-title pricing-card-title">300 PLN</h1>
-                            <ul className="list-unstyled mt-3 mb-4">
-                                <li>przeciwciala Ig-G</li>
-                                <li>przeciwciala Ig-M</li>
-                                <li>Skutecznosc 90%</li>
-                            </ul>
-                            <button type="button" className="btn btn-lg btn-block btn-primary">Zapisz sie</button>
-                        </div>
-                    </div>
-                    <div className="card mb-4 box-shadow">
-                        <div className="card-header">
-                            <h4 className="my-0 font-weight-normal">AIDS</h4>
-                        </div>
-                        <div className="card-body">
-                            <h1 className="card-title pricing-card-title">150 PLN</h1>
-                            <ul className="list-unstyled mt-3 mb-4">
-                                <li>Certyfikowane przez WHO</li>
-                                <li>Leukocyty HCX</li>
-                                <li>Skutecznosc 99%</li>
-                            </ul>
-                            <button type="button" className="btn btn-lg btn-block btn-primary">Zapisz sie</button>
-                        </div>
-                    </div>
-                    <div className="card mb-4 box-shadow">
-                        <div className="card-header">
-                            <h4 className="my-0 font-weight-normal">Ebola</h4>
-                        </div>
-                        <div className="card-body">
-                            <h1 className="card-title pricing-card-title">50 PLN
-                            </h1>
-                            <ul className="list-unstyled mt-3 mb-4">
-                                <li>Tylko na zamowienie</li>
-                                <li>15% znizki w zakladzie pogrzebowym Swiety Spokoj</li>
-                                <li>Skutecznosc 100%</li>
-                            </ul>
-                            <button type="button" className="btn btn-lg btn-block btn-primary">Zapisz sie</button>
-                        </div>
-                    </div>
+                    { this.state.filteredExaminations.map(exam =>
+                        <div className="card mb-4 box-shadow" key={exam.id}>
+                            <div className="card-header">
+                                <h4 className="my-0 font-weight-normal">{exam.name}</h4>
+                            </div>
+                            <div className="card-body">
+                                <h1 className="card-title pricing-card-title">{exam.pricePLN} PLN</h1>
+                                <ul className="list-unstyled mt-3 mb-4">
+                                    {exam.details.map((detail, key) => <li key={key}>{detail}</li>)}
+                                </ul>
+                                <button type="button" className="btn btn-lg btn-block btn-primary">Zapisz sie</button>
+                            </div>
+                        </div>)}
                 </div>
             </div>
         );
